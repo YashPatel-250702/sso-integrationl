4d8dedc1ff10371c1d1dae474f8b5f87de8d606d
YashPatel-250702 <yash.patel@tekworks.in>
2025-07-17 16:04:43 +0530

    addec ocurse

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -10,13 +10,13 @@ const localLogout = () => {
 };
 
 const handleCourseClick = (id: number): void => {
-   window.open(`http://localhost/moodle/course/view.php?id=${id}`,'_blank');
+   window.open(`http://13.233.130.50/bitlabs/courseview.php?id=${id}`,'_blank');
 }
   return (
     <div>
         <h1>Dashboard</h1>
         <h3>Welcome to dashboard</h3>
-        <button onClick={() => handleCourseClick(2)}>HTML Course </button>
+        <button onClick={() => handleCourseClick(7)}>HTML Course </button>
          <button onClick={() => handleCourseClick(3)}>Springboot Course </button>
         <button onClick={localLogout}>log out</button>
     </div>
